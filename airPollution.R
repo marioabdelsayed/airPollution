bee67cb0a7ee880c72b50ed322b5588d5eb547f8
marioabdelsayed <marioabdelsayed98@gmail.com>
2022-09-08 22:10:53 -0400

    added complete and cor functions

--- airPollution.R
+++ airPollution.R
@@ -1,6 +1,11 @@
+#takes a directory containing data files, the pollutant to like
+#calculate the mean for and the numeric id of the file(s)
 pollutantmean <- function (directory, pollutant, id= 1:332){
+  #set the working directory to the files
   setwd(directory)
+  #vector to store pollutants
   pollVector <- vector()
+  #determine file name
   for (value in id){
     if(value < 10){
      fileName <- paste0('00',value,'.csv')
@@ -14,19 +19,21 @@ pollutantmean <- function (directory, pollutant, id= 1:332){
     file <- read.csv(fileName)
     pollVector <- append(pollVector,file[[pollutant]])
   }
+  #remove na from vector
   cleanVector <- pollVector[!is.na(pollVector)]
-  print(cleanVector)
+  #calculate the mean
   meanPollutant <- mean(cleanVector)
   meanPollutant
 }
-
-pollutantmean('C:/Users/ACER/OneDrive/Desktop/projects/Project 1 files/specdata/'
-,'nitrate', 23) 
-
+#returns the number of complete cases (rows not containig emptry or na)
 complete <- function(directory, id=1:332){
+  #set working directory
   setwd(directory)
+  #data frame to store the files and their case numbers
   fileList <- data.frame()
+  #fullcases count
   fullCases <- 0
+  #get file name
   for (value in id){
     if(value < 10){
       fileName <- paste0('00',value,'.csv')
@@ -38,15 +45,64 @@ complete <- function(directory, id=1:332){
       fileName <- paste0(value,'.csv')
     }
     file <- read.csv(fileName)
+    #check if rows in file have na values, if not, increase case count
     for (rowNum in 1:nrow(file)){
       if(!any(is.na(file[rowNum,]))){
         fullCases <- fullCases + 1
       }
     }
+    #add the file and its case count to the file list
     fileList <- rbind(fileList,c(value,fullCases))
     fullCases <- 0
   }
+  #appropriately name the list 
   names(fileList) <- c("id","nobs")
-  print(fileList)
+  fileList
+}
+
+corr <- function(directory, threshold = 0){
+  qualifiedFiles <- vector()
+  corrResults <- vector()
+  nitrate <- vector()
+  sulfate <- vector()
+  fullCases <- complete(directory)
+  #determine the the files that meet the threshold
+  for (row in 1: nrow(fullCases)){
+    if (fullCases[row,2] >= threshold) {
+      qualifiedFiles <- append(qualifiedFiles,fullCases[row,1])
+    }
   }
-complete('C:/Users/ACER/OneDrive/Desktop/projects/Project 1 files/specdata/', 1)
+  #get the pollutants from the qualified files
+  if (length(qualifiedFiles) == 0){
+    print('no files meet the threshold')
+    return()
+  }
+  #get the file names and set them appropriately
+  for(file in 1:length(qualifiedFiles)){
+    if(qualifiedFiles[file] < 10){
+      fileName <- paste0('00',qualifiedFiles[file],'.csv')
+    }
+    else if (10<=qualifiedFiles[file] && qualifiedFiles[file]<100){
+      fileName <- paste0('0',qualifiedFiles[file],'.csv')
+    }
+    else{
+      fileName <- paste0(qualifiedFiles[file],'.csv')
+    }
+    #read the file
+    fileContent <- read.csv(fileName)
+    #for every row, get the nitrate and sulate values 
+    for (rowNumber in 1:nrow(fileContent)){
+      if(!any(is.na(fileContent[rowNumber,]))){
+        nitrate <- append(nitrate,fileContent[rowNumber,][["nitrate"]])
+        sulfate <- append(sulfate,fileContent[rowNumber,][["sulfate"]])
+      }
+    }
+    #append the cor of the nitrate and sulate values to the list 
+    corrResults <- append(corrResults,cor(nitrate,sulfate))
+    #reset the pollutant vectors for the next file
+    nitrate <- vector()
+    sulfate <- vector()
+  }
+  corrResults
+}
+
